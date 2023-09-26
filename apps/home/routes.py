ea82706e1de37f432a6a266355959e7435d16868
cesaenzd <cdsaenzsalazar@gmail.com>
2023-09-26 00:27:46 -0600

    Updated api proccess

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -5,13 +5,13 @@ Copyright (c) 2019 - present AppSeed.us
 
 from apps.home import blueprint
 from flask import Flask, render_template, request, jsonify, send_file, make_response
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
 from flask_login import login_required
 from jinja2 import TemplateNotFound
 from PIL import Image
 import io
 
-from skimage.morphology import binary_erosion, rectangle, binary_dilation, disk
+from skimage.morphology import binary_dilation, binary_erosion, erosion, rectangle, disk
 from skimage.color import rgb2gray
 from skimage.io import imread
 import cv2 as cv
@@ -19,18 +19,18 @@ import numpy as np
 import requests
 from io import BytesIO
 import base64
+import pybase64
+
 
 @blueprint.route('/index')
 @login_required
 def index():
-
     return render_template('home/index.html', segment='index')
 
 
 @blueprint.route('/<template>')
 @login_required
 def route_template(template):
-
     try:
 
         if not template.endswith('.html'):
@@ -51,7 +51,6 @@ def route_template(template):
 
 # Helper - Extract current page name from request
 def get_segment(request):
-
     try:
 
         segment = request.path.split('/')[-1]
@@ -63,89 +62,83 @@ def get_segment(request):
 
     except:
         return None
-    
-def process_and_detect_blobs(image):
-    # Convierte la imagen a escala de grises si no lo está
-    if image.shape[2] == 3:
-        img_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    else:
-        img_gray = image
 
-    ret, img_threshold = cv.threshold(img_gray, 190, 255, cv.THRESH_BINARY)
-    des = cv.bitwise_not(img_threshold)
-    contour, hier = cv.findContours(des, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
 
-    for cnt in contour:
-        cv.drawContours(des, [cnt], 0, 255, -1)
+CORS(blueprint, resources={r"/api/api_process_image": {"origins": "http://127.0.0.1:8100"}})
 
-    drawContours = cv.bitwise_not(des)
-    image_blur = cv.blur(np.float32(drawContours), (3, 3), 5)
 
-    binary_erosion = cv.morphologyEx(image_blur, cv.MORPH_ERODE, np.ones((3, 3), np.uint8))
-    binary_dilation = cv.morphologyEx(binary_erosion, cv.MORPH_DILATE, np.ones((3, 3), np.uint8))
-
-    return binary_dilation
-
-CORS(blueprint, resources={r"/api/api_process_image": {"origins": "http://127.0.0.1:5000"}})
 @blueprint.route('/api/api_process_image', methods=['POST'])
 def api_process_image():
-    if 'image' not in request.files:
-        response_data = {'error': 'No es una imagen'}
+    json_request = request.get_json()
+    if 'image' not in json_request:
+        response_data = {'error': 'Wrong request data'}
         response = jsonify(response_data)
         response.headers.add('Access-Control-Allow-Origin', '*')
 
         return response
-    
-    imagen = request.files['image']
-    if imagen.filename == '' or not imagen.filename.endswith(('.jpg', '.jpeg', '.png')):
-        response_data = {'error': 'El archivo seleccionado no es una imagen válida.'}
-        response = jsonify(response_data)
-        response.headers.add('Access-Control-Allow-Origin', '*')
-    
+
     try:
-        image_bytes = BytesIO(imagen.read())
-        image_np = np.asarray(bytearray(image_bytes.read()), dtype=np.uint8)
-        img = cv.imdecode(image_np, cv.IMREAD_COLOR)
+        image_request = Image.open(io.BytesIO(base64.decodebytes(bytes(json_request['image'], "utf-8"))))
+        image_request.save('./image_request.jpeg')
+        img_gray = cv.cvtColor(cv.imread('./image_request.jpeg'), cv.COLOR_BGR2GRAY)
 
-        binary_dilation = process_and_detect_blobs(img)
+        picture, blobs, white, red = define_results(filter_image(img_gray, False), filter_image(img_gray, True))
 
-        if binary_dilation.dtype != np.uint8:
-            binary_dilation = np.uint8(binary_dilation)
-
-        image = np.uint8(255 * binary_dilation)
-
-        params = cv.SimpleBlobDetector_Params()
-        params.filterByArea = True
-        params.minArea = 20
-        # params.maxArea = 2000
-
-        params.filterByCircularity = True
-        params.minCircularity = 0.01
-        # params.maxCircularity = 1
-
-        params.filterByConvexity = True
-        params.minConvexity = 0.1
-
-        params.filterByInertia = True
-        params.minInertiaRatio = 0.01
-
-        detector = cv.SimpleBlobDetector_create(params)
-        keypoints = detector.detect(binary_dilation)
-
-        blank = np.zeros((1, 1))
-        blobs = cv.drawKeypoints(image, keypoints, blank, (255, 0, 0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-        number_of_blobs = len(keypoints)
-
-        retval, buffer = cv.imencode('.png', blobs)
-        image_base64 = base64.b64encode(buffer).decode('utf-8')
-        response = make_response(send_file(io.BytesIO(buffer), mimetype='image/jpeg'))
+        response = jsonify({'all': blobs, 'white': white, 'red': red, 'picture': picture.decode('utf-8')})
         response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Expires'] = number_of_blobs
         return response
-    
+
     except Exception as e:
-        response = jsonify({'error': 'Excepción'})
+        response = jsonify({'error': repr(e)})
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
-   
+
+
+def filter_image(img_gray, is_white_globes):
+    ret, img_threshold = cv.threshold(img_gray, 100 if is_white_globes else 190, 255, cv.THRESH_BINARY)
+    des = cv.bitwise_not(img_threshold)
+    contour, hier = cv.findContours(des, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE)
+    for cnt in contour:
+        cv.drawContours(des, [cnt], 0, 255, -1)
+    draw_contours = cv.bitwise_not(des)
+
+    if is_white_globes:
+        kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
+        res = cv.morphologyEx(draw_contours, cv.MORPH_OPEN, kernel)
+        img_median_blur = cv.medianBlur(draw_contours, 1)
+        img_binary_dilation = binary_dilation(img_median_blur, disk(2))
+        img_erosioned = erosion(img_binary_dilation, disk(5))
+        cv.imwrite('./white_globes.jpeg', 255 * img_erosioned)
+        return cv.imread('./white_globes.jpeg', 0)
+    else:
+        image_blur = cv.blur(np.float32(draw_contours), (3, 3), 5)
+        img_binary_erosion = binary_erosion(image_blur, rectangle(3, 3))
+        img_binary_dilation = binary_dilation(img_binary_erosion, disk(1))
+        cv.imwrite('./all_globes.jpeg', 255 * img_binary_dilation)
+        return cv.imread('./all_globes.jpeg', 0)
+
+
+def define_results(img_all_globes, img_white_globes):
+    params = cv.SimpleBlobDetector_Params()
+    params.filterByArea = True
+    params.minArea = 20
+    params.filterByCircularity = True
+    params.minCircularity = 0.01
+    params.filterByConvexity = True
+    params.minConvexity = 0.1
+    params.filterByInertia = True
+    params.minInertiaRatio = 0.01
+
+    blank = np.zeros((1, 1))
+    detector = cv.SimpleBlobDetector_create(params)
+    keypoints = detector.detect(img_white_globes)
+    white_blobs_count = len(keypoints)
+    keypoints = detector.detect(img_all_globes)
+    all_blobs_count = len(keypoints)
+    all_blobs = cv.drawKeypoints(img_all_globes, keypoints, blank, (255, 0, 0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+    print("Total de glóbulos:", all_blobs_count)
+    print("Glóbulos rojos:", all_blobs_count - white_blobs_count)
+    print("Glóbulos blancos:", white_blobs_count)
+
+    return pybase64.b64encode(all_blobs), all_blobs_count, white_blobs_count, all_blobs_count-white_blobs_count
